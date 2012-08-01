ba9152f2da443c31a3ed8fb11c01df8961bb978d
Al Carter <arcarter@Al-Carter-MBP13.local>
2012-08-01 16:50:02 -0400

    Initial Commit

---
+++ BikeTrace02/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  BikeTrace02
+//
+//  Created by Al Carter on 8/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+
+- (IBAction)showInfo:(id)sender;
+
+@end
